3d4ea958612eb6bc84382a7cfb88d2afc6a001f1
Andy Burch <andrewwburch@gmail.com>
2020-08-02 12:34:56 -0500

    Broke the agenda config, fixed it.

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -18,7 +18,7 @@
       ("Contractor" "`/.config/emacs/icons/tools.svg" nil nil :ascent center)
       ("Exercise" "~/.config/emacs/icons/barbell.svg" nil nil :ascent center)
       ("Finance" "~/.config/emacs/icons/columns.svg" nil nil :ascent center)
-      ("Life" "~/.config/emacs/icons/leaf.svg" nil nil :ascent center)
+      ("todo" "~/.config/emacs/icons/leaf.svg" nil nil :ascent center)
       ("Organization" "~/.config/emacs/icons/folder.svg" nil nil :ascent center)
       ("Party" "~/.config/emacs/icons/beer.svg" nil nil :ascent center)
       ("Repair" "~/.config/emacs/icons/wrench.svg" nil nil :ascent center)))
@@ -27,20 +27,19 @@
 
 (setq org-directory "~/org")
 (setq org-journal-dir "~/org/journal/")
-(setq org-journal-file-format "%Y-%m-%d.org")
+(setq org-journal-file-format "%Y.org")
 (setq org-journal-enable-agenda-integration t)
 (setq org-journal-skip-carryover-drawers t)
-(setq org-agenda-files '(
-                         "~/org/life.org"))(setq org-mu4e-convert-to-html t)
+(setq org-agenda-files '("~/org/todo.org" "~/org/life.org"))
+;; (add-to-list 'org-agenda-files org-journal-dir)
+(setq org-mu4e-convert-to-html t)
 (setq org-mu4e-link-query-in-headers-mode nil)
-
 (setq org-capture-templates
-      '(("t" "todo" entry (file+headline "~/Documents/todo.org" "Tasks")
+      '(("t" "todo" entry (file+headline "~/org/todo.org" "Tasks")
          "* TODO %?\nSCHEDULED: %(org-insert-time-stamp (org-read-date nil t \"+0d\"))\n%a\n")))
-
-(add-to-list 'org-agenda-files org-journal-dir)
 (setq org-agenda-breadcrumbs-separator " ❱ ")
 (setq org-journal-date-format "%A, %B %d %Y")
+(setq org-journal-file-type "yearly")
 (setq org-log-into-drawer "LOGBOOK")
 (setq org-modules '(org-habit))
 
